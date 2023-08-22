90cee65b1f00588cf9b486e7d7cc9fda48766353
Steve Thornton <technission@outlook.com>
2023-08-22 00:00:40 -0500

    Finished first assignment for JavaScript
    Practiced concatenating strings and using expression.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+var Sent1 = "This is the beginning of my string"; //Declared a variable called Sent 1 with a specific string
+var Sent2 = " and this is the end of the string"; //Declared a variable called Sent 2 with a specific string
+document.write(Sent1 + Sent2); //Used the document.write() method correctly to concantenate the strings together from Sent1 and Sent2
+window.alert("3 x 3 is " + 3 * 3); //Used an expression utilizing the window.alert() method
