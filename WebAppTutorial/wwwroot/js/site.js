13fb98e219ab2036af3220a21023f49ba2f6799c
Steve Thornton <technission@outlook.com>
2023-09-28 23:23:38 -0500

    Created a asp.net project

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//Functions to be called onmouseover and onmouseout to change the color of Hello World
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+function underLine() {
+    document.getElementById("my_song").style.textDecoration = "underline";
+}
